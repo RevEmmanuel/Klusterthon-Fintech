6bd3fec9a10064671fa63495ee77788c392c6532
RevEmmanuel <adeolaae1@gmail.com>
2023-11-21 21:48:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// -------------------------
+// In-memory token blacklist
+const tokenBlackList = new Set();
+module.exports = { tokenBlackList };
+// -------------------------
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const globalExceptionHandler = require("./exceptions/GlobalExceptionHandler")
+const authVerification = require("./middleware/authVerification");
+const authRouter = require("./routes/auth");
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://hux-assessment-frontend.vercel.app'],
+    optionsSuccessStatus: 200,
+    methods: "GET, POST, PUT, DELETE",
+}
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.use('/auth', authRouter);
+
+app.get('/auth/current-user', authVerification ,async (req, res, next) => {
+    try {
+        const user = req.user;
+        console.log(user);
+        const currentUser = {
+            id: user.id,
+            username: user.username
+        }
+        res.status(200).json(currentUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.post('/logout', (req, res) => {
+    const token = req.headers.authorization;
+    if (token) {
+        tokenBlackList.add(token.substring(7));
+        res.status(200).json({ message: 'Logged out successfully' });
+    }
+    else {
+        res.status(401).json({ message: 'Token not found' });
+    }
+})
+
+app.use(globalExceptionHandler);
+
+app.listen(port, async () => {
+    console.log(`Starting Sequelize + Express server on port ${port}...`);
+    console.log(`Server is running on port ${port}...`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,62 @@
+const {createNewUser, loginUser, verifyUser} = require("../service/authService");
+const Router = require("express");
+const yup = require('yup');
+const InvalidOtpException = require("../exceptions/InvalidOtpException");
+
+const authRouter = Router();
+
+// validation schema
+const createUserRequest = yup.object().shape({
+    username: yup.string().required('Please enter a username').min(2, 'Please enter a valid username'),
+    email: yup.string().email().required('Please enter an email').min(2, 'Please enter a valid email'),
+    password: yup.string().required('Please enter a password').min(8, 'Password must be a minimum of 8 characters')
+});
+
+const loginRequest = yup.object().shape({
+    email: yup.string().email().required('Please enter an email').min(2, 'Please enter a valid email'),
+    password: yup.string().required('Please enter a password').min(8, 'Password must be a minimum of 8 characters')
+});
+
+authRouter.post('/signup', async (req, res, next) => {
+    try {
+        const requestBody = req.body;
+        await createUserRequest.validate(requestBody);
+        const createdUser = await createNewUser(requestBody);
+        const createUserResponse = {
+            id: createdUser.id,
+            username: createdUser.username,
+            createdAt: createdUser.createdAt,
+            isVerified: createdUser.isVerified,
+            token: createdUser.token
+        }
+        res.status(201).json({ message: 'User registered successfully', createdUser: createUserResponse });
+    } catch (error) {
+        next(error);
+    }
+});
+
+authRouter.post('/login', async (req, res, next) => {
+    try {
+        const loginDto = req.body;
+        await loginRequest.validate(loginDto);
+        const token = await loginUser(loginDto);
+        res.status(200).json({ token });
+    } catch (error) {
+        next(error);
+    }
+})
+
+authRouter.get('/verify/:otp', async (req, res, next) => {
+    try {
+        const otp = req.params.otp;
+        if (!otp) {
+            throw new InvalidOtpException('Please provide an otp');
+        }
+        await verifyUser(otp);
+        res.status(200).json({ message: 'User verified successfully' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = authRouter;
